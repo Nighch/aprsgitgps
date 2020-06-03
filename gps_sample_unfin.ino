55ad34cfda324784d4288b50bbce4aa440760a53
Nighch <40198480+Nighch@users.noreply.github.com>
2020-06-03 20:39:46 +0800

    Update gps_sample_unfin.ino

--- gps_sample_unfin.ino
+++ gps_sample_unfin.ino
@@ -1,4 +1,4 @@
-// Interfacing Arduino with NEO-6M GPS module
+// Interfacing Arduino with NEO-6M GPS module 重工
  
 #include <TinyGPS++.h>           // Include TinyGPS++ library
 #include <SoftwareSerial.h>      // Include software serial library
@@ -11,14 +11,22 @@ TinyGPSPlus gps;
  
 #define S_RX    4                // Define software serial RX pin
 #define S_TX    3                // Define software serial TX pin
- 
+ #define ADC_REFERENCE REF_3V3 //定義輸出的adc孔
+ #define OPEN_SQUELCH false //開啟sql 我們應該用不到
+ boolean gotPacket = false;
 SoftwareSerial SoftSerial(S_RX, S_TX);    // Configure SoftSerial library
- 
+AX25Msg incomingPacket;
+uint8_t *packetData;
 void setup(void) {
   Serial.begin(9600);
   SoftSerial.begin(9600); 
   lcd.begin(16, 2);
-
+  APRS_init (ADC_REFERENCE, OPEN_SQUELCH);
+  
+ //AX25Msg incomingPacket;
+ uint8_t *packetData;
+  APRS_setCallsign("BX4ACP", 1);//設呼號
+  APRS_setDestination("CAR", 0);
 // 閃爍三次
   lcd.backlight();
 
@@ -30,6 +38,32 @@ void setup(void) {
   lcd.print("GPS START");
   delay(3000);
   lcd.clear(); //顯示清除 
+}
+void aprs_msg_callback(struct AX25Msg *msg) {
+  // If we already have a packet waiting to be
+  // processed, we must drop the new one.
+  if (!gotPacket) {
+    // Set flag to indicate we got a packet
+    gotPacket = true;
+
+    // The memory referenced as *msg is volatile
+    // and we need to copy all the data to a
+    // local variable for later processing.
+    memcpy(&incomingPacket, msg, sizeof(AX25Msg));
+
+    // We need to allocate a new buffer for the
+    // data payload of the packet. First we check
+    // if there is enough free RAM.
+    if (freeMemory() > msg->len) {
+      packetData = (uint8_t*)malloc(msg->len);
+      memcpy(packetData, msg->info, msg->len);
+      incomingPacket.info = packetData;
+    } else {
+      // We did not have enough free RAM to receive
+      // this packet, so we drop it.
+      gotPacket = false;
+    }
+  }
 }
  
 void loop() {
@@ -55,8 +89,10 @@ void loop() {
         Serial.println(ly,3);
         locationUpdate((lx),(ly));
       }
-      else
+      else{
         Serial.println("Location Invalid");
+        locationUpdate(120.123456,24.123456);//讓你在房間也能測試
+      }
  
 //      if (gps.altitude.isValid()) {
 //        Serial.print("Altitude   = ");
@@ -171,7 +207,7 @@ void locationUpdate(float flat,float flon) {
      }  
    }
      
-   //APRS_setLat(y);
+   APRS_setLat(y);
    
   Serial.println();
   Serial.println(F("Location Update:"));
@@ -206,4 +242,13 @@ void locationUpdate(float flat,float flon) {
      }
    Serial.print(z);
   Serial.println();
+  APRS_setLat(z);//設定經度
+  APRS_setPower(2);//設定功率
+  APRS_setHeight(4);//設定海拔
+  APRS_setGain(7);//設定天線增益
+  APRS_setDirectivity(0);
+  char *comment = "LibAPRS location update";//發送附帶的文字
+  APRS_sendLoc(comment, strlen(comment));//發送座標出去
+  Serial.println("TX");
+  free(packetData);//清理記憶體
 }}
