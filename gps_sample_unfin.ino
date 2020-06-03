515ec03057ae8c09e172b26547e9623f74c80f83
Nighch <40198480+Nighch@users.noreply.github.com>
2020-06-03 15:52:52 +0800

    Update gps_sample_unfin.ino
    搞定gps座標問題

--- gps_sample_unfin.ino
+++ gps_sample_unfin.ino
@@ -1,3 +1,5 @@
+// Interfacing Arduino with NEO-6M GPS module
+ 
 #include <TinyGPS++.h>           // Include TinyGPS++ library
 #include <SoftwareSerial.h>      // Include software serial library
 #include  "LiquidCrystal_I2C.h"
@@ -6,39 +8,13 @@
 
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 TinyGPSPlus gps;
-
-
-#define ADC_REFERENCE REF_3V3
-#define OPEN_SQUELCH false
+ 
 #define S_RX    4                // Define software serial RX pin
 #define S_TX    3                // Define software serial TX pin
-boolean gotPacket = false;
-AX25Msg incomingPacket;
-uint8_t *packetData;
-
+ 
 SoftwareSerial SoftSerial(S_RX, S_TX);    // Configure SoftSerial library
-
-void aprs_msg_callback(struct AX25Msg *msg) {
-  if (!gotPacket) {
-    gotPacket = true;
-    memcpy(&incomingPacket, msg, sizeof(AX25Msg));
-    if (freeMemory() > msg->len) {
-      packetData = (uint8_t*)malloc(msg->len);
-      memcpy(packetData, msg->info, msg->len);
-      incomingPacket.info = packetData;
-    } else {
-
-      gotPacket = false;
-    }
-  }
-}
-
-
+ 
 void setup(void) {
-  APRS_init(ADC_REFERENCE, OPEN_SQUELCH);
-  APRS_setCallsign("NOCALL", 1);
-  APRS_printSettings();
-  Serial.print(F("Free RAM:     ")); Serial.println(freeMemory());
   Serial.begin(9600);
   SoftSerial.begin(9600); 
   lcd.begin(16, 2);
@@ -55,18 +31,124 @@ void setup(void) {
   delay(3000);
   lcd.clear(); //顯示清除 
 }
-
-
-
+ 
+void loop() {
+ 
+  while (SoftSerial.available() > 0) {
+ 
+    if (gps.encode(SoftSerial.read())) {
+ 
+      if (gps.location.isValid()) {
+        Serial.print("Latitude   = ");
+        Serial.println(gps.location.lat(), 6);
+        Serial.print("Longitude  = ");
+        Serial.println(gps.location.lng(), 6);
+        lcd.setCursor(0, 0);
+        lcd.print("Latitude=");
+        lcd.print(gps.location.lat(), 6);
+        lcd.setCursor(0, 1); 
+        lcd.print("Longitude=");
+        lcd.print(gps.location.lng(), 6);
+        float lx=gps.location.lat();
+        float ly=gps.location.lng();
+        Serial.println(lx,3);
+        Serial.println(ly,3);
+        locationUpdate((lx),(ly));
+      }
+      else
+        Serial.println("Location Invalid");
+ 
+//      if (gps.altitude.isValid()) {
+//        Serial.print("Altitude   = ");
+//        Serial.print(gps.altitude.meters());
+//        Serial.println(" meters");
+//      }
+//      else
+//        Serial.println("Altitude Invalid");
+// 
+//      if (gps.speed.isValid()) {
+//        Serial.print("Speed      = ");
+//        Serial.print(gps.speed.kmph());
+//        Serial.println(" kmph");
+//      }
+//      else
+//        Serial.println("Speed Invalid");
+// 
+//      if (gps.time.isValid()) {
+//        Serial.print("Time (GMT) : ");
+//        if(gps.time.hour() < 10)     Serial.print("0");
+//        Serial.print(gps.time.hour());
+//        Serial.print(":");
+//        if(gps.time.minute() < 10)   Serial.print("0");
+//        Serial.print(gps.time.minute());
+//        Serial.print(":");
+//        if(gps.time.second() < 10)   Serial.print("0");
+//        Serial.println(gps.time.second());
+//      }
+//      else
+//        Serial.println("Time Invalid");
+// 
+//      if (gps.date.isValid()) {
+//        Serial.print("Date       : ");
+//        if(gps.date.day() < 10)      Serial.print("0");
+//        Serial.print(gps.date.day());
+//        Serial.print("/");
+//        if(gps.date.month() < 10)    Serial.print("0");
+//        Serial.print(gps.date.month());
+//        Serial.print("/");
+//        Serial.println(gps.date.year());
+//      }
+//      else
+//        Serial.println("Date Invalid");
+// 
+//      if (gps.satellites.isValid()) {
+//        Serial.print("Satellites = ");
+//        Serial.println(gps.satellites.value());
+//      }
+//      else
+//        Serial.println("Satellites Invalid");
+ delay(1000);
+    }
+ 
+  }
+ 
+}
+char *ftoa(char *a, double f, int precision)
+{
+ long p[] = {0,10,100,1000,10000,100000,1000000,10000000,100000000};
+ 
+ char *ret = a;
+ long heiltal = (long)f;
+ itoa(heiltal, a, 10);
+ while (*a != '\0') a++;
+ *a++ = '.';
+ long desimal = abs((long)((f - heiltal) * p[precision]));
+ itoa(desimal, a, 10);
+ return ret;
+}
 void locationUpdate(float flat,float flon) {
+  // Let's first set our latitude and longtitude.
+  // These should be in NMEA format!
+
+
+// flat, negative == Southern hemisphere
+// flon, negative == western hemisphere
+//  for instance...  43.646808, -116.286437  
+//nmea then is:
+//  43 .. (.646808*60),N
+// 116 .. (.286437*60),W  (w since it was negative)
+// APRS chokes when you send a ,N though... 
+// it also chokes when you send more than 2 decimal places.
+
   int temp;
   char y[13];
   char z[13];
-  
+
 // CONVERT to NMEA.  
   if (flat<0){
      temp=-(int)flat;
      flat=temp*100 - (flat+temp)*60;
+     ftoa((char*)&y,flat,3);   
      //z[10]=',';
      if (flat > 10000) {
        y[8]='S';
@@ -78,6 +160,7 @@ void locationUpdate(float flat,float flon) {
   } else {
      temp=(int)flat;
      flat=temp*100 + (flat-temp)*60;
+     ftoa((char*)&y,flat,3);      
      //z[10]=',';
      if (flat > 10000) {
        y[8]='N';
@@ -88,16 +171,19 @@ void locationUpdate(float flat,float flon) {
      }  
    }
      
-   APRS_setLat(y);
+   //APRS_setLat(y);
    
   Serial.println();
   Serial.println(F("Location Update:"));
   Serial.print(F("Lat: "));
-  Serial.println(y);
+ Serial.print(y);
+  Serial.println();
+  
   
   if (flon<0){
     temp=-(int)flon;
     flon=temp*100 - (flon+temp)*60;
+    ftoa((char*)&z,flon,3);   
      //z[10]=',';
      if (flon > 10000){
        z[8]='W';
@@ -109,6 +195,7 @@ void locationUpdate(float flat,float flon) {
   } else {
     temp=(int)flon;
     flon=temp*100 + (flon-temp)*60;
+    ftoa((char*)&z,flon,3);   
      //z[10]=',';
      if (flon > 10000){
        z[8]='E';
@@ -117,86 +204,6 @@ void locationUpdate(float flat,float flon) {
       z[7]='E';
       z[8]=0; 
      }
-}
-
-    APRS_setLon(z);
-  Serial.print(F("Lon:"));
-  Serial.println(z);
-  char *comment = " JWAprsTrkr";
-  APRS_sendLoc(comment, strlen(comment));
-}
-
-
-void messageExample() {
-  // We first need to set the message recipient
-  APRS_setMessageDestination("AA3BBB", 0);
-  
-  // And define a string to send
-  char *message = "Hi there! This is a message.";
-  APRS_sendMsg(message, strlen(message));
-  
-}
-
-void processPacket() {
-  if (gotPacket) {
-    gotPacket = false;
-    
-    Serial.print(F("Received APRS packet. SRC: "));
-    Serial.print(incomingPacket.src.call);
-    Serial.print(F("-"));
-    Serial.print(incomingPacket.src.ssid);
-    Serial.print(F(". DST: "));
-    Serial.print(incomingPacket.dst.call);
-    Serial.print(F("-"));
-    Serial.print(incomingPacket.dst.ssid);
-    Serial.print(F(". Data: "));
-
-    for (int i = 0; i < incomingPacket.len; i++) {
-      Serial.write(incomingPacket.info[i]);
-    }
-    Serial.println("");
-
-    // Remeber to free memory for our buffer!
-    free(packetData);
-    
-    // Serial.print(F("Free RAM: ")); Serial.println(freeMemory());
-  }
-}
-
-boolean whichExample = false;
-
-
-void loop() {
-
-   while (SoftSerial.available() > 0) {
- 
-    if (gps.encode(SoftSerial.read())) {
- 
-      if (gps.location.isValid()) {
-         if (whichExample) {
-        locationUpdate((gps.location.lat(), 6),(gps.location.lng(), 6));
-        } else {
-         messageExample();
-        }
-        whichExample ^= true;
-        delay(500);
-        processPacket();
-
-        
-        Serial.print("Latitude   = ");
-        Serial.println(gps.location.lat(), 6);
-        Serial.print("Longitude  = ");
-        Serial.println(gps.location.lng(), 6);
-        lcd.setCursor(0, 0);
-        lcd.print("Latitude=");
-        lcd.print(gps.location.lat(), 6);
-        lcd.setCursor(0, 1); 
-        lcd.print("Longitude=");
-        lcd.print(gps.location.lng(), 6);
-      }
-      else
-        Serial.println("Location Invalid");
-    }
-   }
- delay(1000);
-    }
+   Serial.print(z);
+  Serial.println();
+}}
